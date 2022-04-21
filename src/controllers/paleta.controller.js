861231e83709af7f628e855f12b9fbdc26de46ec
anabeatrizalmeida <ana-almeida@hotmail.com>
2022-04-21 14:37:33 -0300

    add middleware

--- src/controllers/paleta.controller.js
+++ src/controllers/paleta.controller.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 const findAllPaletasController = async (req, res) => {
   const allPaletas = await paletasService.findAllPaletasService();
-
   if (allPaletas.length == 0) {
     return res
       .status(404)
@@ -14,16 +13,7 @@ const findAllPaletasController = async (req, res) => {
 
 const findByIdPaletaController = async (req, res) => {
   const idParam = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    res
-      .status(400)
-      .send({ message: 'ID inválido!' });
-    return;
-  }
-  
   const chosenPaleta = await paletasService.findByIdPaletaService(idParam);
-
   if (!chosenPaleta) {
     return res.status(404).send({ message: 'Paleta não encontrada!' });
   }
@@ -32,67 +22,23 @@ const findByIdPaletaController = async (req, res) => {
 
 const createPaletaController = async (req, res) => {
   const paleta = req.body;
-
-  if (
-    !paleta ||
-    !paleta.sabor ||
-    !paleta.descricao ||
-    !paleta.foto ||
-    !paleta.preco
-  ) {
-    return res
-      .status(400)
-      .send(
-        'Você não preencheu todos os dados para adicionar uma nova paleta ao cardápio!',
-      );
-  }
-
   const newPaleta = await paletasService.createPaletaService(paleta);
   res.status(201).send(newPaleta);
 };
 
 const updatePaletaController = async (req, res) => {
   const idParam = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    res
-      .status(400)
-      .send({ message: 'ID inválido!' });
-    return;
-  }
-
   const editPaleta = req.body;
-
-  if (
-    !editPaleta ||
-    !editPaleta.sabor ||
-    !editPaleta.descricao ||
-    !editPaleta.foto ||
-    !editPaleta.preco
-  ) {
-    return res
-      .status(400)
-      .send(
-        'Você não preencheu todos os dados para adicionar uma nova paleta ao cardápio!',
-      );
-  }
-
-  const updatedPaleta = await paletasService.updatePaletaService(idParam, editPaleta);
+  const updatedPaleta = await paletasService.updatePaletaService(
+    idParam,
+    editPaleta,
+  );
   res.send(updatedPaleta);
 };
 
 const deletePaletaController = async (req, res) => {
   const idParam = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    res
-      .status(400)
-      .send({ message: 'ID inválido!' });
-    return;
-  }
-
   await paletasService.deletePaletaService(idParam);
-
   res.send({ message: 'Paleta deletada com sucesso!' });
 };
 
